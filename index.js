00f001967ef7875cd1ee1b8231dc5167243d10a6
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2014-12-23 13:16:04 -0800

    twilio part working

---
+++ index.js
@@ -0,0 +1,27 @@
+// Configuration variables
+var config = require('./config.json');
+var temperature = 212;
+var message = 'Temperature ' + temperature + 'ºF reached.';
+
+// Require node modules
+var twilio = require('twilio')(config.account_sid, config.auth_token);
+
+sendText();
+
+function sendText() {
+  console.log('Sending text...');
+  twilio.sms.messages.create({
+    to: config.num_to_text,
+    from: config.twilio_num,
+    body: message
+  }, function(error, message) {
+    if (!error) {
+      console.log('Success! The SID for this SMS message is:');
+      console.log(message.sid);
+      console.log('Message sent on:');
+      console.log(message.dateCreated);
+    } else {
+      console.log('Oops! There was an error.', error);
+    }
+  });
+}
